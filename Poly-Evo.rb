4fad601d6ece5d60249a0b05bd1611523eefe840
Luke McEvoy <caledt@gmail.com>
2008-12-12 13:23:40 +1100

    It doesn't work very well :(

--- Poly-Evo.rb
+++ Poly-Evo.rb
@@ -12,35 +12,43 @@ include Math
 Start_time = Time.now
 
 Max = 100
-Pop = 100
+MaxPop = 10
 gen = 1
 
-cat = ImageList.new("Flower_Hat.jpg")
+cat = ImageList.new("Fruit_Candles.jpg")
 Xlim = cat.cur_image.columns
 Ylim = cat.cur_image.rows
 
-tri = PolyImg.new()
-tri.randomize()
+pop = Array.new(MaxPop) { |img| img = PolyImg.new(); img.randomize(); img.draw(cat); img }
 
-for i in 1..10 do 
-    newtri = tri.copy()
-	#puts newtri.inspect
-
-    newtri.mutate() 
-	canvas = newtri.draw(cat)	
+for i in 1..100000 do 
+    complist = []
+    complist = [rand(MaxPop),rand(MaxPop),rand(MaxPop)] until complist.uniq.length == 3
 	
-	if newtri.dif <= tri.dif then
-	  tri = newtri
+	complist.sort! {|x,y| pop[x].dif <=> pop[y].dif }
+	
+	pop[complist[2]] = pop[complist[1]].copy(pop[complist[0]])
+	pop[complist[2]].mutate()
+	canvas = pop[complist[2]].draw(cat)
+	
+	puts [complist+[pop[complist[2]].dif]].inspect
+
+	if i % 100 == 50 then
 	  sgen = gen.to_s
 	  sgen = '0'+sgen while sgen.length < 5
 	  fn = "out/test#{sgen}.jpg"
       canvas.write(fn)
-	  puts newtri.dif.to_s+' '+fn
+	  puts fn
 	  gen += 1	  
 	end
+
 end
 
 #puts pols.inspect
+
+fn = "out/final.jpg"
+canvas.write(fn)
+
 
 puts "Runtime: #{Time.now - Start_time} sec"
 
--- PolyImg.rb
+++ PolyImg.rb
@@ -3,7 +3,7 @@ class PolyImg
 
   def initialize(tri = nil, dif = 200)    
     if tri == nil then
-      @triangles = Array.new(Max) { |t| t = [[0,0],[0,0],[0,0],[0,0,0]] } 
+      @triangles = Array.new(Max) { |t| t = [[0,0],[0,0],[0,0],[0,0,0],[0]] } 
 	else
 	  @triangles = tri
 	end
@@ -12,28 +12,64 @@ class PolyImg
   
   def randomize()
     @triangles.map! { |t| 
-      x,y = rand(Xlim),rand(Ylim)	
-	  t = [[x,y],[x,y],[x,y],[rand,rand,rand]] 
+      p = randompoint()
+	  t = [p,p,p,randomcol,[0]] 
 	}
+  end
+  
+  def randomcol()
+    [rand,rand,rand]
+  end
+  
+  def randompoint()
+    x,y = rand(Xlim),rand(Ylim)
+    [x,y]
   end
   
   def mutate()
     @triangles.map! { |t| 
+	  m = rand
+	  t[4][0] = 1-t[4][0] if m < 0.01
 	  t[0..2].map! { |v| 
-	    v[0] = [0,v[0]+rand(5)-2,Xlim].sort[1]
-	    v[1] = [0,v[1]+rand(5)-2,Ylim].sort[1]
+	    if t[4][0] == 0 then
+		  v = randompoint()
+		else
+  	      v[0] = [0,v[0]+rand*4-2,Xlim].sort[1]
+	      v[1] = [0,v[1]+rand*4-2,Ylim].sort[1]
+		end
 		v
 	  }
-	  t[3].map! { |c| c=[0,c+(rand-0.5)/100,100].sort[1] }
+	  if t[4][0] == 0 then
+		  t[3] = randomcol()
+      else
+  	    t[3].map! { |c| c=[0,c+(rand-0.5)/100,100].sort[1] }
+  	  end
 	  t
 	}    
   end
   
-  def copy()
-	triangles = Array.new(Max)
-    @triangles.each_index { |i| 
-	  triangles[i] = [@triangles[i][0].dup, @triangles[i][1].dup, @triangles[i][2].dup, @triangles[i][3].dup]
-	}
+  def copy(parent2 = nil)
+	triangles = Array.new(Max) 
+	if parent2 == nil then 
+	    @triangles.each_index { |i| 
+		  triangles[i] = [@triangles[i][0].dup, @triangles[i][1].dup, @triangles[i][2].dup, @triangles[i][3].dup]
+		}
+	else 
+	    @triangles.each_index { |i| 
+		  triangles[i] = [[0,0],[0,0],[0,0],[0,0,0],[0]]
+		  @triangles[i].each_index { |i2|
+			@triangles[i][i2].each_index { |i3|
+			  if i2 == 4 then #Visible
+			    triangles[i][i2][i3] = [@triangles[i][i2][i3],parent2.triangles[i][i2][i3]].max
+			  else
+			    #puts [i,i2,i3,triangles[i][i2][i3],parent2.triangles[i][i2][i3]].inspect
+		        triangles[i][i2][i3] = (@triangles[i][i2][i3] + parent2.triangles[i][i2][i3])/2
+			  end
+			}
+		  }
+		}
+	end
+	
     newimg = PolyImg.new(triangles, @dif)
     
   end
@@ -43,15 +79,19 @@ class PolyImg
 	tridraw.stroke_width(0)
 	
 	@triangles.each { |tri| 
+      if tri[4][0] > 0 then 	
 		tridraw.fill("rgb(#{tri[3][0]*100}%,#{tri[3][1]*100}%,#{tri[3][2]*100}%)")
 		tridraw.fill_opacity(0.3)
 		tridraw.polygon(tri[0][0],tri[0][1],tri[1][0],tri[1][1],tri[2][0],tri[2][1])
+	  end
 	}
 
 	r,g,b = @triangles[0][3][0],@triangles[0][3][1],@triangles[0][3][2]
 	canvas = Magick::ImageList.new
 	canvas.new_image(Xlim, Ylim) {  
-	  self.background_color = "rgb(#{r*100}%,#{g*100}%,#{b*100}%)" 
+#	  self.background_color = "rgb(#{r*100}%,#{g*100}%,#{b*100}%)" 
+	  self.background_color = "white" 
+#	  self.background_color = "black" 
 	}
 	
 	#puts tridraw.inspect	
